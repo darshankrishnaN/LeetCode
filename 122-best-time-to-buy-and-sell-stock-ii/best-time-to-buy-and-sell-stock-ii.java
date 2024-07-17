ebf511f9d264e27a68024f6fd6568236e4cffd96
Darshan Krishna N <143327911+darshankrishnaN@users.noreply.github.com>
2024-07-17 10:16:02 +0530

    Time: 1 ms (89.98%) | Memory: 45.5 MB (75.50%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -3,9 +3,10 @@ class Solution {
        int profit =0;
        for(int i=0;i<prices.length-1;i++){
         if(prices[i+1]>prices[i]){
-            profit+=prices[i+1]-prices[i];
+              profit+=prices[i+1]-prices[i];
         }
-       }
-       return profit;
     }
+    return profit;
     }
+}
+
