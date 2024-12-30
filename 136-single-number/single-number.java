b3c2a4b3cc369b59c6e31cdea6ebe154d294fc26
Darshan Krishna N <143327911+darshankrishnaN@users.noreply.github.com>
2024-12-30 13:31:26 +0530

    Time: 1 ms (99.87%) | Memory: 46.2 MB (38.46%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,10 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int v=0;
-        for(int i=0;i<nums.length;i++){
-          v = v^nums[i];
-        }
-         return v;
+       int k = 0;
+       for(int i =0;i<nums.length;i++){
+        k = k^nums[i];
+        
        }
-       
+       return k;
     }
+}
