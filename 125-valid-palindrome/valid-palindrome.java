64df8c94c34a7c341efa786e515487472e4aa604
Darshan Krishna N <143327911+darshankrishnaN@users.noreply.github.com>
2025-02-04 12:47:24 +0530

    Time: 14 ms (34.27%) | Memory: 45.3 MB (20.91%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,18 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
-    s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
-    int i = 0;
-    int j = s.length()-1;
+   
+   s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
 
-    while(i<=j){
-        if(s.charAt(i)!=s.charAt(j)){
-            return false;
-        }
-        i++;
-        j--;
+   int i =0;
+   int j = s.length()-1;
+
+   while(i<j){
+
+    if(s.charAt(i)!=s.charAt(j)){
+       return false;
     }
-    return true;
+    i++;
+    j--;
+   }
+   return true;
     }
 }
+
+
+
 
   
