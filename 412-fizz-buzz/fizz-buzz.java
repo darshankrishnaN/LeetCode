71d3d3650b641c7d6a1b758840be44e139ea7dfd
Darshan Krishna N <143327911+darshankrishnaN@users.noreply.github.com>
2025-03-05 14:42:51 +0530

    Time: 1 ms (99.62%) | Memory: 45.4 MB (27.93%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        
+        List<String> ar = new ArrayList<>();
+        
+        int[] ans = new int[n];
+
+        for(int i=1;i<=n;i++){
+
+            if(i%15 == 0){
+                ar.add("FizzBuzz");
+            }
+
+            else if(i%3 == 0 ){
+                ar.add("Fizz");
+            }
+
+           else if( i%5 == 0){
+                ar.add("Buzz");
+            }
+
+            else{
+                ar.add(String.valueOf(i));
+            }
+        }
+
+        return ar;
+        
+    }
+}
