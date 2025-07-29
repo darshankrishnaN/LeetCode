b57b282faa2245502a538317336fe8aef74495f3
Darshan Krishna N <143327911+darshankrishnaN@users.noreply.github.com>
2025-07-29 16:14:02 +0530

    Time: 1 ms (99.14%) | Memory: 45.6 MB (27.14%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.java
+++ 2048-build-array-from-permutation/build-array-from-permutation.java
@@ -1,12 +1,15 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-    int[] ans = new int[nums.length]; // Initialize the answer array
+     
+     int[] ans = new int[nums.length];
+     int k=0;
+     for(int i=0;i<=nums.length-1;i++){
+        
+        ans[k] = nums[nums[i]];
+        k++;
+        
 
-    for (int i = 0; i < nums.length; i++) {
-        int value = nums[i]; // Get value from nums at index i
-        ans[i] = nums[value]; // Use value as an index to get nums[value]
-    }
-
-    return ans;
+     }
+     return ans;
 }
 }
