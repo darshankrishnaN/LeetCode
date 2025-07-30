59a775efce143a92627f4068c5880f3edf425ce8
Darshan Krishna N <143327911+darshankrishnaN@users.noreply.github.com>
2025-07-30 11:30:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (86.42%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -1,16 +1,24 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
         
-   
-        int max =0;
-        for(int i=0;i<accounts.length;i++){
-                 int sum =0;
-            for(int j=0;j<accounts[i].length;j++){
+   int sum =0;
+   int max = 0;
 
-             sum +=  accounts[i][j];
-            }
-                max = Math.max(sum,max);
-        }
-         return max;
+  
+   for(int i=0;i<accounts.length;i++)  //row
+   {
+      sum=0;
+    for(int j =0;j<accounts[i].length;j++) // col
+    {   
+     
+        sum +=  accounts[i][j];
+     
+
+    }
+       max = Math.max(sum,max);
+       
+   }
+       return max;
     }
 }
+
