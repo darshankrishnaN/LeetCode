cff50834e675579f59f543ccbb9943bd8952fbf8
Darshan Krishna N <143327911+darshankrishnaN@users.noreply.github.com>
2025-11-21 12:48:36 +0530

    Time: 34 ms (17.53%) | Memory: 46.9 MB (18.68%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -4,13 +4,11 @@ class Solution {
         
         for(int i=0;i<s.length();i++){
             
-            if(m.containsKey(s.charAt(i))){
+         
                  m.put(s.charAt(i),m.getOrDefault(s.charAt(i),0)+1);
 
-            }
-            else{
-                m.put(s.charAt(i),1);
-            }
+            
+       
         }
 
       for (int i = 0; i < s.length(); i++) {
