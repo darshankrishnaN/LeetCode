a26b77866bbd2fa0aac0d5fa69c04ff1816864fb
Darshan Krishna N <143327911+darshankrishnaN@users.noreply.github.com>
2025-11-21 12:42:42 +0530

    Time: 41 ms (5.83%) | Memory: 47 MB (9.06%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -3,28 +3,21 @@ class Solution {
         HashMap<Character,Integer> m = new HashMap<>();
         
         for(int i=0;i<s.length();i++){
-
+            
             if(m.containsKey(s.charAt(i))){
-                m.put(s.charAt(i),2);
-            }
+                 m.put(s.charAt(i),m.getOrDefault(s.charAt(i),1)+1);
 
+            }
             else{
                 m.put(s.charAt(i),1);
             }
         }
-            
-     for(int i=0;i<s.length();i++){
 
-        if(1 == m.get(s.charAt(i))){
-
-            return i;
+      for (int i = 0; i < s.length(); i++) {
+            if (m.get(s.charAt(i)) == 1) {
+                return i;
+            }
         }
-     }
-
-        
         return -1;
-          
-           
-
-        }
     }
+}
