267d6b597a060f9771ef2264b2fea80b2dbe2d0b
Darshan Krishna N <143327911+darshankrishnaN@users.noreply.github.com>
2025-12-05 13:06:36 +0530

    Time: 1 ms (60.50%) | Memory: 43.3 MB (14.66%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -5,18 +5,23 @@ class Solution {
 
     String str1 = strs[0];
     String str2 = strs[strs.length-1];
-    int i=0;
 
-        while(i<str1.length() && str1.charAt(i) == str2.charAt(i)){
-            i++;
-        }
-   
+    int i =0;
 
-        if(i==0){
-            return "";
-        }
-        else{
-            return str1.substring(0,i);
-        }
+    while(i<str1.length() && str1.charAt(i)==str2.charAt(i)){
+        i++;
+    }
+    
+    if(i==0){
+        return "";
+    }
+    else{
+    
+    return str1.substring(0,i);
+}
+
     }
 }
+
+
+    
